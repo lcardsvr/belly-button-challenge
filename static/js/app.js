1b86878bac044bcb2c8bf26d00bf1b17861f79e6
Luis Cardenas <lcardsvr@gmail.com>
2023-03-26 20:55:48 +0800

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -27,6 +27,8 @@ function init (){
 
     plotMetaData(metadata[0]);
 
+    fillGaugeChart(metadata[0]);
+
 
 
   });
@@ -42,7 +44,7 @@ function plotBarChart (samples){
     let numSamples = (samples.sample_values.slice(0,10));
     numSamples = numSamples.reverse();
     
-    let axis = samples.otu_ids.slice(0,10).map(sample => `OTC ${sample}`);
+    let axis = samples.otu_ids.slice(0,10).map(sample => `OTU ${sample}`);
     axis = axis.reverse();
  
 
@@ -66,7 +68,7 @@ function plotBarChart (samples){
    
    // Apply a title to the layout
    let layout = {
-     title: "Top 10 OTUs",
+     title: "<b>Top 10 OTUs</b>",
      margin: {
        l: 100,
        r: 100,
@@ -81,8 +83,7 @@ function plotBarChart (samples){
 }
 
 
-//Function to plot the BarCharts
-
+//Function to plot the BarChart
 
 function plotBubbleChart (samples){
     
@@ -113,9 +114,8 @@ function plotBubbleChart (samples){
       
       // Apply a title to the layout
       let layout = {
-        title: "Bacteria Per Sample",
         hovermode: "closest",
-        xaxis: {title: "OTU ID"}
+        xaxis: {title: "<b>OTU ID</b>"}
       };
       
       Plotly.newPlot("bubble", traceData, layout);
@@ -128,20 +128,16 @@ function plotBubbleChart (samples){
 function plotMetaData (metadata){
 
     // Create an array of category labels
-    let dataLabels = Object.keys(metadata);
-
-        
+    let dataLabels = Object.keys(metadata);        
     // Create an array with Metadata Values
     let dataValues = Object.values(metadata);
 
 
     console.log("Labels " + dataLabels);
-
     console.log("values " + dataValues);
 
 
     // Clear previous contents
-
     d3.select("#sample-metadata").html("");
 
 
@@ -159,11 +155,10 @@ function plotMetaData (metadata){
 }
 
 //Function to fill the dropdowns with all the names
-
 function init_dropdown (names){
 
 
-  // Function called by DOM changes
+  // Checking current Test Subject ID
   let dropdownMenu = d3.select("#selDataset");
 
 
@@ -176,9 +171,11 @@ function init_dropdown (names){
 
 }
 
+
+//Function that is run everytime there is a change in teh TEst Subject ID
 function optionChanged (){
 
-    // Function called by DOM changes
+    // Checking current Test Subject ID
     let dropdownMenu = d3.select("#selDataset");
 
     // Assign the value of the dropdown menu option to a variable the metadata id and sample id
@@ -197,6 +194,8 @@ function optionChanged (){
       plotBubbleChart(samples[dataset]);
    
       plotMetaData(metadata[dataset]);
+
+      fillGaugeChart (metadata[dataset]);
    
    });
 
@@ -204,3 +203,66 @@ function optionChanged (){
   }
 
   init();
+
+
+//Function to generate teh Gauge Chart
+  function fillGaugeChart(metadata){
+
+    // Create an array of category labels
+    let dataLabels = Object.keys(metadata);
+
+        
+    // Create an array with Metadata Values
+    let dataValues = Object.values(metadata);
+
+
+    console.log("Labels " + dataLabels);
+
+    console.log("values " + dataValues);
+
+
+    let washingFrequency = dataValues[6];
+
+            // Set up the trace for the gauge chart
+            //Colors taken from https://learn.microsoft.com/en-us/power-platform/power-fx/reference/function-colors
+            let trace = {
+              value: washingFrequency,
+              domain: {x: [0,1], y: [0,1]},
+              title: {
+                  text: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week",
+                  // font: {color: "black", size: 16}
+                  font: {color: "black"}
+              },
+              type: "indicator",
+              mode: "gauge+number",
+              gauge: {
+                  axis: {range: [0,9], tickmode: "linear", tick0: 1, dtick: 1},
+                  steps: [
+                      {range: [0, 1], color: "rgba(240, 248, 255, 1)"},
+                      {range: [1, 2], color: "rgba( 250, 235, 215, 1 )"},
+                      {range: [2, 3], color: "rgba( 250, 235, 215, 1 )"},
+                      {range: [3, 4], color:  "rgba(250, 250, 210, 1)"},
+                      {range: [4, 5], color:  "rgba(211, 211, 211, 1)"},
+                      {range: [5, 6], color: "rgba(102, 205, 170, 1 )"},
+                      {range: [6, 7], color: "rgba(0, 250, 154, 1 )"},
+                      {range: [7, 8], color:  "rgba( 60, 179, 113, 1)"},
+                      {range: [8, 9], color: "rgba(107, 142, 35, 1)"},
+                      
+                  ]
+              } 
+          };
+  
+          // Set up the Layout
+          let layout = {
+              width: 400, 
+              height: 400,
+              margin: {t: 0, b:0}
+          };
+  
+          // Call Plotly to plot the gauge chart
+          Plotly.newPlot("gauge", [trace], layout)
+      
+  };
+
+
+
